f22e69f14ede1613fcabaaa614a4c5a978841948
Omar El Sahlah <98979833+FullStackin@users.noreply.github.com>
2023-07-16 12:02:35 -0700

    Login Form completed

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -1,65 +1,45 @@
-// frontend/src/components/LoginFormModal/index.js
-import React, { useState, useEffect } from "react";
+// frontend/src/components/LoginFormPage/index.js
+import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
-import { useDispatch } from "react-redux";
-import { useModal } from "../../context/Modal";
-import "./LoginForm.css";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
 
-function LoginFormModal() {
+function LoginFormPage() {
   const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
   const [credential, setCredential] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
-  const { closeModal } = useModal();
-  const [disabled, setDisabled] = useState(true);
+
+  if (sessionUser) return <Redirect to="/" />;
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setErrors({});
-    return dispatch(sessionActions.login({ credential, password }))
-      .then(closeModal)
-      .catch(async (res) => {
+    return dispatch(sessionActions.login({ credential, password })).catch(
+      async (res) => {
         const data = await res.json();
-        if (data && data.errors) {
-          setErrors(data.errors);
-        }
-      });
-  };
-
-  useEffect(() => {
-    if (credential.length >= 4 && password.length >= 6) {
-      setDisabled(false);
-    } else {
-      setDisabled(true);
-    }
-  }, [credential, password]);
-
-  const loginDemo = () => {
-    dispatch(
-      sessionActions.login({ credential: "Demo-lition", password: "password" })
-    ).then(closeModal);
+        if (data && data.errors) setErrors(data.errors);
+      }
+    );
   };
 
   return (
     <>
-      <form className="form-container" onSubmit={handleSubmit}>
-        <h1 className="logIn">Log In</h1>
-        <label className="text-input">
-          {/* Username or Email */}
+      <h1>Log In</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username or Email
           <input
-            className="input credentials"
-            placeholder="Username or Email"
             type="text"
             value={credential}
             onChange={(e) => setCredential(e.target.value)}
             required
           />
         </label>
-        <label className="text-input">
-          {/* Password */}
+        <label>
+          Password
           <input
-            className="input password"
-            placeholder="Password"
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
@@ -67,21 +47,10 @@ function LoginFormModal() {
           />
         </label>
         {errors.credential && <p>{errors.credential}</p>}
-        <button
-          className={
-            disabled ? "submit-button-inactive" : "submit-button-login"
-          }
-          type="submit"
-          disabled={disabled}
-        >
-          Log In
-        </button>
-        <button className="demo-button" onClick={loginDemo}>
-          DemoUser Login
-        </button>
+        <button type="submit">Log In</button>
       </form>
     </>
   );
 }
 
-export default LoginFormModal;
+export default LoginFormPage;
