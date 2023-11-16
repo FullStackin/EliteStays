82706bd7e39901c13fd32cb6a4bde787b52f0231
Omar El Sahlah <98979833+FullStackin@users.noreply.github.com>
2023-11-16 10:59:51 -0800

    adjusted css styling for front page

--- frontend/src/components/AllSpotsItem/index.jsx
+++ frontend/src/components/AllSpotsItem/index.jsx
@@ -1,36 +1,29 @@
 import "./AllSpotsItem.css";
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 function AllSpotsItem({ spot }) {
   let priceDisplay = Math.round(spot.price);
   let rating = Math.round(Number(spot.avgRating) * 10) / 10;
-  let ratingDisplay = " " + rating.toFixed(1);
-  if (ratingDisplay == 0) {
-    ratingDisplay = " New";
-  } else {
-    ratingDisplay = Number(ratingDisplay).toFixed(1);
-  }
+  let ratingDisplay = rating ? rating.toFixed(1) : "New";
 
   return (
     <div className="spot-item-container">
       <div title={spot.name} className="spot-item">
-        {" "}
         <NavLink className="spot-item-link" exact to={`/spots/${spot.id}`}>
           <h2>{spot.name}</h2>
           <img src={spot.previewImage} />
           <div className="spot-info">
-            <div className="spot-info-row1">
-              <h3>
-                {spot.city},{" " + spot.state}
-              </h3>
-              <h3>
-                <i className="fa-solid fa-star"></i>
-                {ratingDisplay}
-              </h3>
+            <h3>{spot.city}, {spot.state}</h3>
+            <div className="price-rating">
+              <h4><strong>${priceDisplay}</strong> a night</h4>
+              <div className="rating">
+                <i className="fa-solid fa-star"><FontAwesomeIcon icon={faStar} />
+</i>
+                <span>{ratingDisplay}</span>
+              </div>
             </div>
-            <h4>
-              <strong>${priceDisplay}</strong> a night
-            </h4>
           </div>
         </NavLink>
       </div>
